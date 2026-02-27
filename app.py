cfb302ad0db0a0f4ac27e92483bf83579b6456f1
GouthamiV24 <gouthamiyadu05@gmail.com>
2026-02-27 09:11:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask,render_template
-
 app=Flask(__name__)
-
 @app.route("/",methods=["GET"])
-def test():
+def home():
     return render_template("index.html")
 
-if __name__=='__main__':
-    app.run(debug=True)
+@app.route("/get-data",methods=["GET"])
+def get_data():
+    @app.route("/get-data", methods = ["GET"])
+    db = mysql.connector.connect(
+        host="localhost",
+        user= "root",
+        password= "root",
+        database= "flask_app"
+    )
+
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users_auth")
+
+    data = cursor.fetchall()
+
+    return jsonify(data)
+if __name__=="__main__":
+    app.run(debug=True) 
+   
